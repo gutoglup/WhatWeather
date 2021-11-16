6b66f05624263f0313996b9bd1967ed57e935ca7
Augusto Reis <reis-augusto@hotmail.com>
2021-11-16 09:47:12 -0300

    Initial Commit

---
+++ WhatWeather/WhatWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatWeatherApp.swift
+//  WhatWeather
+//
+//  Created by Augusto Reis on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WhatWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
