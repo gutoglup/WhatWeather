4bd8124684cff2b8e778db85983d770a66ed07b5
Augusto Reis <reis-augusto@hotmail.com>
2022-08-09 11:08:26 -0300

    Add color extension

---
+++ WhatWeather/Core/Extensions/Color+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Extension.swift
+//  WhatWeather
+//
+//  Created by Augusto Reis on 08/08/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryBrand = Color("Primary")
+    static let secondaryBrand = Color("Secondary")
+    static let tertiaryBrand = Color("Tertiary")
+    static let quartenaryBrand = Color("Quartenary")
+    static let quintenaryBrand = Color("Quintenary")
+    static let neutralBrand = Color("Neutral")
+}
